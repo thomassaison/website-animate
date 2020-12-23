62394d1c775d47219ab32136f088a4184d6e79bf
thomas.saison <thomas.saison@seraphin.legal>
2020-12-23 12:11:46 +0100

    first commit

---
+++ js/site.js
@@ -0,0 +1,27 @@
+let toSearch = "jumbotron";
+let toAdd = "shadow-pop-tr";
+let toRemove = "shadow-pop-tr-reverse";
+
+function handleScroll() {
+  let wTop = $(this).scrollTop();
+  let wHeight = $(window).height();
+  let wBottom = wTop + wHeight;
+  $("." + toSearch).each(function (index) {
+    let cTop = $(this).offset().top;
+    let cBottom = cTop + $(this).outerHeight();
+    if (wTop <= cTop && wBottom >= cBottom) {
+        if ($(this).hasClass(toRemove))
+            $(this).removeClass(toRemove);
+        if (!$(this).hasClass(toAdd))
+            $(this).addClass(toAdd);
+    } else {
+        if (!$(this).hasClass(toRemove))
+            $(this).addClass(toRemove);
+    }
+  });
+}
+
+handleScroll();
+$(window).scroll(() => {
+    handleScroll();
+});
